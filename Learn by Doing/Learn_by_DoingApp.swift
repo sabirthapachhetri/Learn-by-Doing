1149829eea978ae00f64828e110f5cdd3c7f69df
Sabir <sabirthapa19@gmail.com>
2022-11-01 15:32:59 +0545

    Initial Commit

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Sabir's MacBook on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
